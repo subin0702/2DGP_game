c8927c551a307e8f7cf9f940d28a7cb96dd58c9f
subin0702 <tbasl0702@naver.com>
2021-10-19 21:34:38 +0900

    map구현

---
+++ animation/map/map.py
@@ -0,0 +1,39 @@
+from pico2d import *
+
+KPU_WIDTH, KPU_HEIGHT = 1280, 1024
+
+class Ground:
+    def __init__(self):
+        self.image = load_image('ground.png')
+    def draw(self):
+        self.image.draw(640, 250)
+
+
+def handle_events():
+    global mapping
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            mapping = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            mapping = False
+
+#open_canvas()
+open_canvas(1280,1024)
+back_ground = load_image('deep_forest_2.jpg')
+
+ground = Ground()
+
+mapping = True
+
+while mapping:
+    handle_events()
+    clear_canvas()
+    back_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+    ground.draw()
+
+    update_canvas()
+
+    delay(0.05)
+
+close_canvas()
