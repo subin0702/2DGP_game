4289382c8d48d33497af25de84891a7fb47a2478
subin0702 <tbasl0702@naver.com>
2021-10-19 22:50:10 +0900

    class 로 나타내기

--- animation/character/ingame.py
+++ animation/character/ingame.py
@@ -2,19 +2,18 @@ from pico2d import *
 
 class BoyCharacter:
     def __init__(self):
-        self.x, self.y = 0, 30
+        self.x, self.y = 0, 90
         self.frame = 0
         self.image = load_image('boyanimation.png')
 
     def update(self):
-        #walking
         self.frame = (self.frame + 1) % 8
         self.x += 5
 
 
     def draw(self):
-        self.frame = (self.frame*100, 1400, 100, 100, self.x, self.y)
-pass
+        self.image.clip_draw = (self.frame*100, 1400, 100, 100, self.x, self.y)
+
 
 def handle_events():
     global running
@@ -37,20 +36,19 @@ open_canvas()
 boy = BoyCharacter()
 
 running = True
+
+# game main loop code
 while (running) :
     handle_events()
 
     boy.update()
 
     clear_canvas()
-    #character.clip_draw(frame*100, 1400, 100, 100, x, 90)
-    #update_canvas()
-    #frame = (frame + 1) % 8
-    #x += 5
     boy.draw()
+
     update_canvas()
+
     delay(0.03)
-    get_events()
 
 close_canvas()
 """
