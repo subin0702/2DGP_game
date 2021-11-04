1969a947682e7d2b2c241c947c8d04db50c1a3df
subin0702 <tbasl0702@naver.com>
2021-11-04 16:35:18 +0900

    title 진행

---
+++ animation/tilte/start_state.py
@@ -0,0 +1,58 @@
+import game_framework
+from pico2d import *
+
+import title_state
+
+
+name = "StartState"
+image = None
+logo_time = 0.0
+
+
+def enter():    # image 로드해줌
+    global image
+    image = load_image('kpu_credit.png')
+    pass
+
+
+def exit():     # 끝날때 image를 날림
+    global image
+    del(image)
+    pass
+
+
+def update():   # logo play time 쌓이다가 1.0이 지나면 종료
+    global logo_time
+
+    if (logo_time > 1.0):
+        logo_time = 0
+        # game_framework.quit()
+        game_framework.change_state(title_state)    # title과 start를 연결해줘야함
+    delay(0.01)
+    logo_time += 0.01
+    pass
+
+
+def draw():
+    global image
+    clear_canvas()
+    image.draw(400, 300)
+    update_canvas()
+    pass
+
+
+
+
+def handle_events():
+    events = get_events()
+    pass
+
+
+def pause(): pass
+
+
+def resume(): pass
+
+
+
+
